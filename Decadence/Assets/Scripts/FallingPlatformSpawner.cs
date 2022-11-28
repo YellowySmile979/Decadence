7d8657e0afc6115e5c2dd6e85ee0a3ce5968e857
JH-AFK <NGJH.22@ichat.sp.edu.sg>
2022-11-28 13:17:02 +0800

    Just finished adding the platformFallingSpawner

--- Decadence/Assets/Scripts/FallingPlatformSpawner.cs
+++ Decadence/Assets/Scripts/FallingPlatformSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class FallingPlatformSpawner : MonoBehaviour
 {
+    public GameObject fallingplatformPrefab;
+    [HideInInspector] public bool isplatformThere;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isplatformThere = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isplatformThere)
+        {
+            StartCoroutine(SpawnPlatform());
+            isplatformThere = true;
+
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public IEnumerator SpawnPlatform()
+    {
+        yield return new WaitForSeconds(3f);
+
+        GameObject platform = Instantiate(fallingplatformPrefab, transform.position, transform.rotation);
+
+        Destroy(gameObject, 0.5f);
+
     }
 }
