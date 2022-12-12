6cce35f03fc409aa520732288b60cf388d892a27
YellowySmile979 <xcant5@gmail.com>
2022-12-12 18:58:52 +0800

    act2 level 1 more stuf added\

--- Decadence/Assets/Scripts/World/FallingPlatform.cs
+++ Decadence/Assets/Scripts/World/FallingPlatform.cs
@@ -6,14 +6,14 @@ public class FallingPlatform : MonoBehaviour
 {
     Rigidbody2D rb;
     FallingPlatformSpawner FPS;
-    
+    EdgeCollider2D ec;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        FPS = GetComponentInParent<FallingPlatformSpawner>(); 
-
+        FPS = GetComponentInParent<FallingPlatformSpawner>();
+        ec = GetComponent<EdgeCollider2D>();
 
     }
 
@@ -25,14 +25,21 @@ public class FallingPlatform : MonoBehaviour
         //if collided game object with the platform has the tag "Player" 
         {
             Invoke("DropPlatform", 0.5f);
-            //cause a method to occur in seconds 
+            //cause a method to occur in seconds
 
+            Invoke("DisableCollider", 0.5f);
+            
             Destroy(gameObject, 3f);
 
             FPS.isplatformThere = false;
 
             
         }
+    }
+    void DisableCollider()
+    {
+        ec.enabled = false; //disables edge collider
+
     }
     void DropPlatform()
     {
