5a18538a93639f9b324690bd58162ca05290c1b9
YellowySmile979 <xcant5@gmail.com>
2022-12-12 23:34:21 +0800

    whole crap ton of changes

--- Decadence/Assets/Scripts/Game/PopUp.cs
+++ Decadence/Assets/Scripts/Game/PopUp.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PopUp : MonoBehaviour
 {
     MeshRenderer mr;
-    public bool thugHasDied = true;
 
     // Start is called before the first frame update
     void Start()
@@ -13,42 +12,22 @@ public class PopUp : MonoBehaviour
         mr = GetComponent<MeshRenderer>();
 
     }
-    //when player collides with collider, enable the mesh renderer, assuming the thug hasn't died yet
+    //when player collides with collider, enable the mesh renderer
     void OnTriggerEnter2D(Collider2D other)
     {
-        //from the code written under void OnTriggerExit2D, if thugHasDied == true, the mesh renderer will turn on as per normal,
-        //otherwise the mesh renderer will permanently be set to false
-        if (thugHasDied == true)
+        if (other.GetComponent<PlayerController>())
         {
+            mr.enabled = true;
 
-            if (other.GetComponent<PlayerController>())
-            {
-                mr.enabled = true;
-
-            }
-        }else if (thugHasDied == false)
+        }        
+    }
+    //when player interacts with collider, msg will appear
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>())
         {
             mr.enabled = false;
 
-        }
-    }
-    //when player interacts with collider, msg will appear. if thug dies, msg is PERMANENTLY not able to show up
-    void OnTriggerExit2D(Collider2D other)
-    {
-        //determines if thug has died or not and if he has died, sets the bool to false PERMANENTLY
-        if (thugHasDied == true)
-        {
-            if (other.GetComponent<PlayerController>())
-            {
-                mr.enabled = false;
-
-            }
-            else if (other.GetComponent<HurtPlayer>())
-            {
-                mr.enabled = false;
-                thugHasDied = false;
-
-            }
         }
     }
 }
---
+++ Decadence/Assets/Scripts/Game/PopUpThug.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpThug : MonoBehaviour
+{
+    MeshRenderer mr;
+    public bool thugHasDied = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+
+    }
+    //when player collides with collider, enable the mesh renderer, assuming the thug hasn't died yet
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //from the code written under void OnTriggerExit2D, if thugHasDied == true, the mesh renderer will turn on as per normal,
+        //otherwise the mesh renderer will permanently be set to false
+        if (thugHasDied == true)
+        {
+
+            if (other.GetComponent<PlayerController>())
+            {
+                mr.enabled = true;
+
+            }
+        }else if (thugHasDied == false)
+        {
+            mr.enabled = false;
+
+        }
+    }
+    //when player interacts with collider, msg will appear. if thug dies, msg is PERMANENTLY not able to show up
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //determines if thug has died or not and if he has died, sets the bool to false PERMANENTLY
+        if (thugHasDied == true)
+        {
+            if (other.GetComponent<PlayerController>())
+            {
+                mr.enabled = false;
+
+            }
+            else if (other.GetComponent<EnemyController>())
+            {
+                mr.enabled = false;
+                thugHasDied = false;
+
+            }
+        }
+    }
+}
