1e5afd87cde38403cee333202c57e788de522165
YellowySmile979 <xcant5@gmail.com>
2022-12-13 18:29:59 +0800

    some more bug fixes

--- Decadence/Assets/Scripts/Game/Act1BulletCollecting.cs
+++ Decadence/Assets/Scripts/Game/Act1BulletCollecting.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletCollecting : MonoBehaviour
+public class Act1BulletCollecting : MonoBehaviour
 {
     //sets the value the bullet adds as ammo
     public int bulletAmmoValue = 1;
 
     Weapon weapon;
+    Level2Transition lt;
 
     //when player collides with bullet, ammo is added and is destroyed
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.GetComponent<PlayerController>())
+        if (other.GetComponent<PlayerController>())
         {
             weapon.AddAmmo(bulletAmmoValue); //adds ammo
+
+            lt.BulletPickupCounter(bulletAmmoValue); //counts towards the final bullet counter so that nothing goes wrong
 
             Destroy(gameObject); //part that destroys the object
 
@@ -24,6 +27,7 @@ public class BulletCollecting : MonoBehaviour
     void Start()
     {
         weapon = FindObjectOfType<Weapon>();
+        lt = FindObjectOfType<Level2Transition>();
 
     }
 }
--- Decadence/Assets/Scripts/World/Level2Transition.cs
+++ Decadence/Assets/Scripts/World/Level2Transition.cs
@@ -6,15 +6,47 @@ using UnityEngine.SceneManagement;
 
 public class Level2Transition : MonoBehaviour
 {
+    public int requiredNumberOfBullets = 1;
+    public int numberOfBullets;
+
+    MeshRenderer mr;
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+
+    }
+
     //upon entering the trigger, the trigger activates
     void OnTriggerEnter2D(Collider2D other)
     //prevents other colliders from loading scenes like bullets
     {
-        if (other.GetComponent<PlayerController>())
+        //only activates if both the player AND all the required bullets have been collected
+        if (other.GetComponent<PlayerController>() && numberOfBullets >= requiredNumberOfBullets) 
         {
             //loads scene named level 2
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("Act 2 Level 1");
 
         }
+        //enables the MeshRenderer if player steps into the collider
+        if(other.GetComponent<PlayerController>())
+        {
+            mr.enabled = true;
+
+        }
+    }
+    //when player exits the collider, disable popup
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerController>())
+        {
+            mr.enabled = false;
+
+        }
+    }
+    public void BulletPickupCounter(int number)
+    {
+        numberOfBullets += number;
+
     }
 }
