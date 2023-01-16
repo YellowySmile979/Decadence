b016d50db3b4e3c28e8ae11748701af6c8e0fa6e
YellowySmile979 <xcant5@gmail.com>
2023-01-16 23:01:07 +0800

    more

--- Decadence/Assets/Scripts/Enemy/EnemyController.cs
+++ Decadence/Assets/Scripts/Enemy/EnemyController.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public int health = 2;
+    int checkForHalfHealth;
     int enemyIncreaseNumber = 1;
+    public Sprite bloodied;
 
 
     PopUpExit puExit;
+    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         puExit = FindObjectOfType<PopUpExit>();
-        
+        checkForHalfHealth = health / 2; //sets this variable to be half health
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //when half health switch the sprite
+        if(health == checkForHalfHealth)
+        {
+            sr.sprite = bloodied;
+        }
     }
     //when bullet collides with collider, enemy's health is reduced. if enemies health is equal to or less than 0,
     //increase number of enemies still needed to be killed and perform Die() 
