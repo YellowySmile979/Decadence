a100eea15ed2c0b389ab52aab8998b006d5b2b7e
ae-therx <ANGELINAENG.22@ichat.sp.edu.sg>
2023-01-17 01:26:18 +0800

    Update DialogueTrigger.cs

--- Decadence/Assets/Scripts/World/DialogueTrigger.cs
+++ Decadence/Assets/Scripts/World/DialogueTrigger.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool diatrigger = false;
+    public TMP_Text dialoguelines;
+    public GameObject theTextbox;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.GetComponent<PlayerController>())
+        {
+            diatrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            diatrigger = false; 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(diatrigger == true && Input.GetKeyDown(KeyCode.E))
+        {
+            theTextbox.SetActive(true);
+            dialoguelines.gameObject.SetActive(true);
+        }
+        else
+        {
+            theTextbox.SetActive(false);
+        }
     }
 }
