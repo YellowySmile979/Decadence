c003d90e88da589fd33d09b2e71b8d3f5d75be1d
YellowySmile979 <xcant5@gmail.com>
2023-01-16 23:28:46 +0800

    push

--- Decadence/Assets/Scripts/Game/TableFlip.cs
+++ Decadence/Assets/Scripts/Game/TableFlip.cs
@@ -9,6 +9,7 @@ public class TableFlip : MonoBehaviour
 
     bool isPlayerOnIt = false;
 
+    //when player is on the trigger, set the variable to true so to say that player is on it
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<PlayerController>())
@@ -16,6 +17,7 @@ public class TableFlip : MonoBehaviour
             isPlayerOnIt = true;
         }
     }
+    //when player is not on the trigger, set the variable to false so to say that player is not on it
     void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.GetComponent<PlayerController>())
@@ -26,6 +28,7 @@ public class TableFlip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if the player is on it and the player presses E, disable this table and enable the other table
         if(isPlayerOnIt == true && Input.GetKeyDown(KeyCode.E))
         {
             enabledTable.SetActive(false);
--- Decadence/Assets/Scripts/Game/TableShieldDestroy.cs
+++ Decadence/Assets/Scripts/Game/TableShieldDestroy.cs
@@ -7,11 +7,12 @@ public class TableShieldDestroy : MonoBehaviour
     public float tableHP = 3;
     public GameObject enabledTable;
     
+    //checks to see if specifically bullets from either the player or the enemy and then minus health
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.GetComponent<Projectile>())
         {
-            tableHP--;
+            tableHP--; //miuses hp by 1
         }
         if(collision.collider.GetComponent<EnemyProjectile>())
         {
@@ -21,6 +22,7 @@ public class TableShieldDestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if table hp reaches zero destroy the original table and this one
         if(tableHP <= 0)
         {
             Destroy(enabledTable);
